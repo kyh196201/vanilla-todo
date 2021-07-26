a79e5695024d3ffa1a0083f4ef557457a653b97a
kyh196201 <kyh196201@gmail.com>
2021-07-26 15:50:59 +0900

    eslint 설치 및 설정

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['html', 'import', 'eslint-plugin-prettier'],
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: false,
+        printWidth: 80,
+        tabWidth: 2,
+        semi: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
+    'no-unused-vars': 'error',
+  },
+};
