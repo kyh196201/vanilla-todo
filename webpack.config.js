38ed62c787c90bbeda5d819950831e0d8eb02449
seungwoo Kim <kyh196201@gmail.com>
2021-08-07 10:28:08 +0900

    webpack development, production 분기

--- webpack.config.js
+++ webpack.config.js
@@ -3,93 +3,106 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/js/index.js',
-  },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    // https://code.visualstudio.com/docs/languages/jsconfig#_using-webpack-aliases
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-      Components: path.resolve(__dirname, 'src/js/components/'),
-      Store: path.resolve(__dirname, 'src/js/store/'),
-      Api: path.resolve(__dirname, 'src/js/api/'),
-      Utils: path.resolve(__dirname, 'src/js/utils/'),
-      Core: path.resolve(__dirname, 'src/js/core/'),
-      Config: path.resolve(__dirname, 'src/js/config/'),
-      Lib: path.resolve(__dirname, 'src/js/lib/'),
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  const isProduction = argv.mode === 'production';
+  let projectTitle = '';
+
+  if (isDevelopment) {
+    projectTitle = 'Development';
+  } else if (isProduction) {
+    projectTitle = 'vanilla-todo-app';
+  }
+
+  return {
+    // mode: 'development',
+    entry: {
+      main: './src/js/index.js',
     },
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    writeToDisk: true,
-    port: 9000,
-    open: true,
-    hot: true,
-    compress: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
+    output: {
+      filename: '[name].[hash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+      // https://code.visualstudio.com/docs/languages/jsconfig#_using-webpack-aliases
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+        Components: path.resolve(__dirname, 'src/js/components/'),
+        Store: path.resolve(__dirname, 'src/js/store/'),
+        Api: path.resolve(__dirname, 'src/js/api/'),
+        Utils: path.resolve(__dirname, 'src/js/utils/'),
+        Core: path.resolve(__dirname, 'src/js/core/'),
+        Config: path.resolve(__dirname, 'src/js/config/'),
+        Lib: path.resolve(__dirname, 'src/js/lib/'),
+      },
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      writeToDisk: true,
+      port: 9000,
+      open: true,
+      hot: true,
+      compress: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                additionalData: `
+                @import './_variables.scss';
+              `,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.m?js$/i,
+          include: [path.resolve(__dirname, 'src/js')],
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              additionalData: `
-              @import './_variables.scss';
-            `,
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      browsers: ['last 2 versions', 'ie >= 11'],
+                    },
+                  },
+                ],
+              ],
+              plugins: [
+                ['@babel/plugin-proposal-class-properties'],
+                ['@babel/plugin-transform-runtime', {corejs: 3}],
+              ],
             },
           },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.m?js$/i,
-        include: [path.resolve(__dirname, 'src/js')],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    browsers: ['last 2 versions', 'ie >= 11'],
-                  },
-                },
-              ],
-            ],
-            plugins: [
-              ['@babel/plugin-proposal-class-properties'],
-              ['@babel/plugin-transform-runtime', {corejs: 3}],
-            ],
-          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: projectTitle,
+        template: './index.html',
+        filename: 'index.html',
+      }),
+      new CleanWebpackPlugin(),
+      new webpack.DefinePlugin({
+        API_URL: JSON.stringify('http://localhost:3000/'),
+        'process.env.NODE_ENV': JSON.stringify('development'),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'vanilla-todo-app',
-      template: './index.html',
-      filename: 'index.html',
-    }),
-    new CleanWebpackPlugin(),
-    new webpack.DefinePlugin({
-      API_URL: JSON.stringify('http://localhost:3000/'),
-    }),
-  ],
+  };
 };
