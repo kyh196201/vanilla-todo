e98a978a96bf7f6cedcd3913f7feb6ec5bb47ceb
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2021-08-08 16:29:45 +0900

    1. checkbox svg 추가 2. todo-item 템플릿 마크업 변경

--- src/js/utils/template/todoTemplate.js
+++ src/js/utils/template/todoTemplate.js
@@ -1,44 +1,31 @@
-const checkboxTemplate = ({title, isCompleted}) => {
+const checkboxTemplate = ({id, title, isCompleted}) => {
   const $checkbox = isCompleted
-    ? `<input type="checkbox" class="todo-item__checkbox" checked>`
-    : `<input type="checkbox" class="todo-item__checkbox" />`;
-
-  const $title = isCompleted ? `<strike>${title}</strike>` : title;
+    ? `<input type="checkbox" id="${id}" class="todo-item__checkbox" checked>`
+    : `<input type="checkbox" id="${id}" class="todo-item__checkbox" />`;
 
   return `
-	  <div class="todo-item__checkbox-wrapper pretty p-default">
-		${$checkbox}
-		<div class="state p-danger">
-			<label>
-			  <span class="todo-item__title">
-				${$title}
-			  </span>
-			</label>
-		</div>
-	  </div>
-	`;
+	${$checkbox}
+	<label class="todo-item__label ${isCompleted ? 'completed' : ''}" for="${id}">
+		<p class="todo-item__title">${title}</p>
+	</label>
+  `;
 };
-
-const editInputTemplate = title => `
-	  <div class="todo-item__edit-wrapper">
-		<input type="text" class="todo-item__edit" value="${title}">
-	  </div>
-	`;
 
 const editTemplate = todo => {
   const {id, title} = todo;
-  const $inputTemplate = editInputTemplate(title);
 
   return `
       <li class="todo-item" data-id="${id}">
-        ${$inputTemplate}
-		<div class="todo-item__btns">
-			<button type="button" aria-label="done" class="todo-item__btn todo-item__btn--done">
-				<i class="fas fa-check"></i>
-			</button>
-			<button type="button" aria-label="redo" class="todo-item__btn todo-item__btn--redo">
-				<i class="fas fa-redo-alt"></i>
-			</button>
+	  	<div class="todo-item__top">
+			<input type="text" class="todo-item__edit" value="${title}">
+			<div class="todo-item__btns">
+				<button type="button" aria-label="done" class="todo-item__btn todo-item__btn--done">
+					<i class="fas fa-check"></i>
+				</button>
+				<button type="button" aria-label="redo" class="todo-item__btn todo-item__btn--redo">
+					<i class="fas fa-redo-alt"></i>
+				</button>
+			</div>
 		</div>
       </li>`;
 };
@@ -50,14 +37,16 @@ const todoTemplate = todo => {
 
   return `
       <li class="todo-item" data-id="${id}">
-        ${$checkbox}
-		<div class="todo-item__btns">
-			<button type="button" aria-label="edit" class="todo-item__btn todo-item__btn--edit">
-				<i class="fas fa-edit"></i>
-			</button>
-			<button type="button" aria-label="delete" class="todo-item__btn todo-item__btn--delete">
-				<i class="fas fa-times"></i>
-			</button>
+	  	<div class="todo-item__top">
+			${$checkbox}
+			<div class="todo-item__btns">
+				<button type="button" aria-label="edit" class="todo-item__btn todo-item__btn--edit">
+					<i class="fas fa-edit"></i>
+				</button>
+				<button type="button" aria-label="delete" class="todo-item__btn todo-item__btn--delete">
+					<i class="fas fa-times"></i>
+				</button>
+			</div>
 		</div>
       </li>`;
 };
